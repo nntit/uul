6b061adb6c88fbf483ec03dc47a7b06b62e34005
Thanh <nnt1289@live.com>
2022-05-18 16:47:16 +0700

    update

--- examples/index.js
+++ examples/index.js
@@ -1,5 +1,22 @@
 const uul = require('../');
-const uws = uul.uws;
-const App = uws.App({});
+const renderView = require('./src/renderView');
+const readBody = require('./src/readBody');
+const sendFile = require('./src/sendFile');
+const staticFiles = require('./src/staticFiles');
 
-uul.staticFiles(App, '/*', path.resolve(__dirname, './dist'), 'index.html');
+const App = new uul({});
+const uws = App.uws;
+
+uws.get('/*', (res, req) => {
+    /* It does Http as well */
+    res.writeStatus('200 OK').writeHeader('IsExample', 'Yes').end('Hello there!');
+});
+App.get('/test/*', (res, req) => {
+    /* It does Http as well */
+    res.writeStatus('200 OK').writeHeader('IsExample', 'Yes').end('Hello there!');
+});
+App.listen(9001, (listenSocket) => {
+    if (listenSocket) {
+        console.log('Listening to port 9001');
+    }
+});
--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const uws = require('uWebSockets.js');
+const uWebSockets = require('uWebSockets.js');
 const renderView = require('./src/renderView');
 const readBody = require('./src/readBody');
 const sendFile = require('./src/sendFile');
 const staticFiles = require('./src/staticFiles');
 
-const uwu = {
-    uws,
-    renderView,
-    readBody,
-    sendFile,
-    staticFiles,
-};
+class uul {
+    #uws;
+    constructor(options = {}) {
+        if (options == null || typeof options !== 'object')
+            throw new Error(' constructor only accepts an object type for the options parameter.');
+        const { cert_file_name, key_file_name } = options;
+        if (cert_file_name && key_file_name) {
+            this.#uws = uWebSockets.SSLApp(options);
+        } else {
+            this.#uws = uWebSockets.App(options);
+        }
+    }
+    get uws() {
+        return this.#uws;
+    }
 
-module.exports = uwu;
+    get() {
+        this.#uws.get(...arguments);
+    }
+    listen() {
+        this.#uws.listen(...arguments);
+    }
+}
+
+module.exports = uul;
